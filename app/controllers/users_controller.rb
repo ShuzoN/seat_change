82ef485c809fb189936d4f7572fc4453b25297b2
ShuzoN <d.namu.r21@gmail.com>
2016-08-12 21:29:11 +0900

    [modify] 画像のアップロード機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :nick_name, :email,:password, :password_confirmation, :image, :remember_digest))
+      params.require(:user).permit(:name, :nick_name, :image)
     end
 end
