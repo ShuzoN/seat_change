6c94d05aafb10fd9c4ace618a1222b8f0f355d21
ShuzoN <d.namu.r21@gmail.com>
2016-08-12 23:09:48 +0900

    rootにuser iconを表示する

--- app/controllers/seat_position_controller.rb
+++ app/controllers/seat_position_controller.rb
@@ -2,7 +2,7 @@ class SeatPositionController < ApplicationController
   before_action :get_users, only: [:show, :edit, :update]
 
   def index
-
+    @users = User.all.order(:id)
   end
 
   def get_users
--- app/helpers/seat_position_helper.rb
+++ app/helpers/seat_position_helper.rb
@@ -1,2 +1,8 @@
 module SeatPositionHelper
+
+  def newline?(line_num)
+    return true if line_num%8==0
+    return false
+  end
+
 end
