b70d3c0a48c87036f12de86dc488306b60cbaa15
ShuzoN <d.namu.r21@gmail.com>
2016-08-12 22:29:44 +0900

    userからnameを削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812113833) do
+ActiveRecord::Schema.define(version: 20160812132908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20160812113833) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "nick_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
