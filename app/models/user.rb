8eb5da2df77a7f9d7c8bfe04824c584a27037a6e
ShuzoN <d.namu.r21@gmail.com>
2016-08-14 00:44:48 +0900

    positionの初期化を行う処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
 class User < ActiveRecord::Base
-  after_initialize :set_default_value
+  after_initialize :set_default_position
 
   mount_uploader :image, ImageUploader
   validates_presence_of :nick_name, :image, :position 
   validates_uniqueness_of :nick_name
   validates :image, format: { with: /[\w-]+\.jpg|jpeg|gif|png/ }
 
-  def get_recent_position
+  def get_latest_position
+    return 0 if User.all.empty?
+    return User.order("position DESC").first.position+1
   end
 
   private
-  def set_default_value
-    self.position ||= 1
+  def set_default_position
+    self.position ||= get_latest_position
   end
 end
